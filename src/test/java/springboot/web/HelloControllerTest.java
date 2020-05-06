816889d801252d4e8221b54abbef0896eeefb526
jeonghoon <tghyyhju@gmail.com>
2020-05-06 15:17:50 +0900

    update

--- src/test/java/springboot/web/HelloControllerTest.java
+++ src/test/java/springboot/web/HelloControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -28,14 +27,14 @@ public class HelloControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	@WithMockUser(roles = "USER")
+	
 	@Test
 	public void hello_리턴된다() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.content().string("hello"));
 	}
-	@WithMockUser(roles = "USER")
+	
 	@Test
 	public void helloDto가_리턴된다() throws Exception {
 		final String name = "hello";
