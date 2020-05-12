8766059b87e3aa500984c158d578a50b73918f6c
jeonghoon <tghyyhju@gmail.com>
2020-05-12 10:16:32 +0900

    stopsh

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,13 +3,13 @@
 # 쉬고 있는 프로파일 찾기
 function find_idle_profile()
 {
-	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/profile)
+	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 	
 	if [ ${RESPONSE_CODE} -ge 400 ]
  	then
  		CURRENT_PROFILE=real2
  	else
- 		CURRENT_PROFILE=real1
+ 		CURRENT_PROFILE=$(curl -s http://localhost/profile) # real1 또는 real2
  	fi
  	
  	if [ ${CURRENT_PROFILE} == real1 ]
